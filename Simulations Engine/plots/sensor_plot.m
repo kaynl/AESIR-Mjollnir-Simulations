6d94aeb9779ad726b3398a37ac1dded1ec773e40
Kim <kim.lamboley@hotmail.fr>
2021-02-22 19:20:59 +0100

    Sensor Prediction

---
+++ Simulations Engine/plots/sensor_plot.m
@@ -0,0 +1,49 @@
+%% Sensor measurments calculations
+
+
+%% Sensor Vent
+
+P_sensor_vent = 
+T_sensor_vent = 
+
+%% Sensor Tank
+
+P_sensor_tank_top = 
+P_sensor_tank_mid = 
+P_sensor_tank_low =
+
+
+T_sensor_tank_top = 
+T_sensor_tank_mid = 
+T_sensor_tank_low =
+
+
+%% Sensor kastrullan
+
+P_sensor_kast_top =
+
+T_sensor_kast_top =
+T_sensor_kast_mid =
+
+
+%% Sensor Injector
+
+P_sensor_inj =
+
+T_sensor_inj_top =
+T_sensor_inj_low =
+
+
+%% Sensor CC
+
+P_sensor_CC =
+
+T_sensor_CC_top =
+T_sensor_CC_fuel =
+T_sensor_CC_low =
+
+%% Sensor Nozzle
+
+
+T_sensor_throat =
+
